a0b72e60775d9084607c323c131037d7812685b2
Choi Goon-ho <caiguanhao@gmail.com>
2014-01-21 23:51:42 +0800

    move footer js code to file;

---
+++ static.静态/google-analytics.js
@@ -0,0 +1,90 @@
+var alllanglinks=document.getElementById('alllangs').getElementsByTagName('a');
+for(var i=0; i<alllanglinks.length; i++) {
+alllanglinks[i].setAttribute('href', alllanglinks[i].getAttribute('href')+window.location.pathname.replace(/^\/lang\/(.+?)\//, '/').substring(1));
+}
+
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+
+try {
+var pageTracker = _gat._getTracker("UA-2235061-4");
+pageTracker._trackPageview();
+} catch(err) {}
+
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+
+try {
+var pageTracker = _gat._getTracker("UA-2235061-7");
+pageTracker._trackPageview();
+} catch(err) {}
+
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+
+try {
+var pageTracker = _gat._getTracker("UA-2235061-8");
+pageTracker._trackPageview();
+} catch(err) {}
+
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+
+try {
+var pageTracker = _gat._getTracker("UA-2235061-9");
+pageTracker._trackPageview();
+} catch(err) {}
+
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+
+try {
+var pageTracker = _gat._getTracker("UA-2235061-10");
+pageTracker._trackPageview();
+} catch(err) {}
+
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+
+try {
+var pageTracker = _gat._getTracker("UA-2235061-11");
+pageTracker._trackPageview();
+} catch(err) {}
+
+
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+
+try {
+var pageTracker = _gat._getTracker("UA-2235061-12");
+pageTracker._trackPageview();
+} catch(err) {}
+
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+
+try {
+var pageTracker = _gat._getTracker("UA-2235061-13");
+pageTracker._trackPageview();
+} catch(err) {}
+
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+
+try {
+var pageTracker = _gat._getTracker("UA-2235061-14");
+pageTracker._trackPageview();
+} catch(err) {}
+
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+
+try {
+var pageTracker = _gat._getTracker("UA-2235061-15");
+pageTracker._trackPageview();
+} catch(err) {}
+
+
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F8908804cfff3bceeae8ae46c516f1077' type='text/javascript'%3E%3C/script%3E"));
+
