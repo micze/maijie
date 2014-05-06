6f63446cc2733cbb6e43c8a008220b54ef7236ec
micze <huangyuxiong@gmail.com>
2014-05-06 21:18:01 +0800

    Angle
    Angle

--- static.静态/google-analytics.js
+++ static.静态/google-analytics.js
@@ -18,14 +18,6 @@ var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "htt
 document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
 
 try {
-var pageTracker = _gat._getTracker("UA-2235061-8");
-pageTracker._trackPageview();
-} catch(err) {}
-
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-
-try {
 var pageTracker = _gat._getTracker("UA-2235061-9");
 pageTracker._trackPageview();
 } catch(err) {}
@@ -46,36 +38,11 @@ var pageTracker = _gat._getTracker("UA-2235061-11");
 pageTracker._trackPageview();
 } catch(err) {}
 
-
 var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
 document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
 
 try {
 var pageTracker = _gat._getTracker("UA-2235061-12");
-pageTracker._trackPageview();
-} catch(err) {}
-
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-
-try {
-var pageTracker = _gat._getTracker("UA-2235061-13");
-pageTracker._trackPageview();
-} catch(err) {}
-
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-
-try {
-var pageTracker = _gat._getTracker("UA-2235061-14");
-pageTracker._trackPageview();
-} catch(err) {}
-
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-
-try {
-var pageTracker = _gat._getTracker("UA-2235061-15");
 pageTracker._trackPageview();
 } catch(err) {}
 
